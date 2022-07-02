de504f58950e62f9d3d0fd92f63848c6dacd3089
Никита Тристень <nick.tristen22@gmail.com>
2022-07-03 00:13:36 +0300

    init project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-blog/' : '/',
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: '@import "~@/assets/css/variables.scss";'
+            }
+        }
+    }
 })
